0a2fa05e44dd5f44fbfb441e6afd19086e59930f
shane tully <shane@shanetully.com>
2013-07-07 15:21:17 -0700

    fixed package_data, added tests, and bumped version

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ from setuptools import find_packages
 
 setup(
     name='Cryptully',
-    version='1.0.0',
+    version='2.1.0',
     author='Shane Tully',
     author_email='shane@shanetully.com',
     url='https://github.com/shanet/Cryptully',
     license='LGPL3',
     description='An encrypted chat program for those that don\'t know crypto',
+    test_suite='cryptully.tests',
     packages=find_packages(),
     package_data={
-        'cryptully': ['src/images/*']
+        'cryptully': ['images/*.png', 'images/light/*.png', 'images/dark/*.png']
     },
     install_requires=[
-        'Qt4',
         'M2Crypto'
+        # PyQt4 is also required, but it doesn't play nicely with setup.py
     ],
 )
