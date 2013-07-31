02a9d9028aa194ae0bc21a1eec9ef570411b482b
shane tully <shane@shanetully.com>
2013-07-30 19:39:57 -0700

    refactored fingerprint dialog layout

--- cryptully/qt/qFingerprintDialog.py
+++ cryptully/qt/qFingerprintDialog.py
@@ -3,6 +3,7 @@ from PyQt4.QtGui  import QDialog
 from PyQt4.QtGui  import QHBoxLayout
 from PyQt4.QtGui  import QIcon
 from PyQt4.QtGui  import QLabel
+from PyQt4.QtGui  import QPixmap
 from PyQt4.QtGui  import QVBoxLayout
 
 from qLine import QLine
@@ -17,27 +18,35 @@ class QFingerprintDialog(QDialog):
         self.setWindowTitle("Verify Key Integrity")
         self.setWindowIcon(QIcon("images/placeholder.png"))
 
+        keyIcon = QLabel(self)
+        keyIcon.setPixmap(QPixmap(qtUtils.getAbsoluteImagePath('fingerprint.png')).scaledToWidth(80, Qt.SmoothTransformation))
+
+
         helpLabel = QLabel("In order to ensure that no one is listening in on your conversation it's\n"
                            "best to verify the following information with your friend over a secure\n"
                            "line of communication, like a telephone. It's okay if someone sees or\n"
                            "hears this info; it just matters if what they see here matches what\n"
-                           "you see. If not, someone could be listening to your communcations!")
+                           "you see. If not, someone could be listening to your conversation!")
 
-        localLabel = QLabel("You read to them:")
-        localFingerprintLabel = QLabel(crypto.getLocalFingerprint())
-        remoteLabel = QLabel("They read to you:")
-        remoteFingerprintLabel = QLabel(crypto.getRemoteFingerprint())
+        # Replace the semicolons with spaces to make it look less intimidating
+        localFingerprintLabel = QLabel('You read to them: <b>' + crypto.getLocalFingerprint().replace(':', ' ') + '</b>')
+        remoteFingerprintLabel = QLabel('They read to you: <b>' + crypto.getRemoteFingerprint().replace(':', ' ') + '</b>')
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(keyIcon)
+        hbox.addSpacing(15)
+        hbox.addWidget(helpLabel)
+        hbox.addStretch(1)
 
         vbox = QVBoxLayout()
         vbox.addStretch(1)
-        vbox.addWidget(helpLabel)
+        vbox.addLayout(hbox)
         vbox.addSpacing(15)
         vbox.addWidget(QLine())
         vbox.addSpacing(15)
-        vbox.addWidget(localLabel)
         vbox.addWidget(localFingerprintLabel, alignment=Qt.AlignHCenter)
-        vbox.addSpacing(45)
-        vbox.addWidget(remoteLabel)
+        vbox.addSpacing(10)
         vbox.addWidget(remoteFingerprintLabel, alignment=Qt.AlignHCenter)
         vbox.addStretch(1)
 
